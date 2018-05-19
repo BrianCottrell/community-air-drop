3fea7fab05925e4b3ca7e2e6ad97e08f9f59f9f0
BrianCottrell <Brian_Cottrell@inbox.com>
2018-05-19 16:43:51 -0700

    Updated map icons

--- main.js
+++ main.js
@@ -1,25 +1,66 @@
 function initMap() {
   var uluru = {lat: 37.383754, lng: -122.012696};
+  var drone = {lat: 37.386754, lng: -122.022696};
+  var purple = {lat: 37.387754, lng: -122.013096};
+  var red = {lat: 37.380754, lng: -122.012096};
+  var blue = {lat: 37.381754, lng: -122.012896};
   var circle;
   var rMin = 5, rMax = 300, step = 8;
+  var direction = 1;
 
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 14,
+    zoom: 15,
     center: uluru
   });
 
+  var infowindow = new google.maps.InfoWindow({
+    content: "<img src='https://static1.squarespace.com/static/52dfbc9fe4b084050d4e2c55/t/52e9069ae4b0e16c45f2f3dc/1391003297780/icon-saf.png'/>"
+  });
+
+  var infowindow1 = new google.maps.InfoWindow({
+    content: "Hello"
+  });
+
+
   var marker = new google.maps.Marker({
-    position: uluru,
+    position: drone,
+    icon: "drone_icon.png",
+    map: map
+  });
+
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+
+  var marker1 = new google.maps.Marker({
+    position: purple,
+    icon: "purple_icon.png",
+    map: map
+  });
+  marker1.setVisible(false);
+  marker1.addListener('click', function() {
+    infowindow1.open(map, marker1);
+  });
+
+  var marker2 = new google.maps.Marker({
+    position: red,
+    icon: "red_icon.png",
+    map: map
+  });
+
+  var marker3 = new google.maps.Marker({
+    position: blue,
+    icon: "blue_icon.png",
     map: map
   });
 
   circle = new google.maps.Circle({
-    center: uluru,
+    center: drone,
     radius: rMax,
-    strokeColor: "#E16D65",
+    strokeColor: "#444499",
     strokeOpacity: 0.5,
     strokeWeight: 2,
-    fillColor: "#E16D65",
+    fillColor: "#9999FF",
     fillOpacity: 0.5
   });
   circle.setMap(map);
@@ -41,6 +82,21 @@ function initMap() {
             radius = rMin
         }
         circle.setRadius(radius + step);
+
+	    var p = circle.getCenter();
+	    var g = p.lat();
+	    var m = p.lng()+0.0001*direction;
+	    if (m > -122.002696) {
+	    	direction = -1;
+	    	g = p.lat()-0.0004;
+	    } else if (m < -122.022696) {
+	    	direction = 1
+	    	g = p.lat()-0.0004;
+	    } else if (m > -122.016096) {
+	    	marker1.setVisible(true);
+	    }
+	    circle.setCenter(new google.maps.LatLng(g,m));
+	    marker.setPosition(new google.maps.LatLng(g-0.0005,m));
     }, 50);        
   }
 }
